28d3511a36b9734703a3c049e9f73765c381bf38
Pedro Grachet <pedro.grachet@gmail.com>
2022-02-14 22:11:49 -0400

    Módulo 2 - Aula 01

--- Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -6,6 +6,55 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTestes
     {
         [Fact]
+        public void LeilaoComTresClientes()
+        {
+            //Arrange - Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var beltrano = new Interessada("beltrano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 990);
+            leilao.RecebeLance(maria, 1000);
+            leilao.RecebeLance(beltrano, 1400);
+
+            //Act - Método sob teste
+            leilao.TerminaPregao();
+
+            //Assert - Resultado
+            var valorEsperado = 1400;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadosPorValor()
+        {
+            //Arrange - Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 990);
+            leilao.RecebeLance(maria, 1000);
+
+            //Act - Método sob teste
+            leilao.TerminaPregao();
+
+            //Assert - Resultado
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
         public void LeilaoComApenasUmLance()
         {
             //Arrange - Cenário
